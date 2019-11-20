32f5cea6e6827419b086432de62812cc6b42e7ca
xvking <13772424255@163.com>
2019-11-20 23:28:59 +0800

    修改

--- test/unit/webppack-base-test.js
+++ test/unit/webppack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'D:/Projects/build-webpack-xv/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'D:/Projects/build-webpack-xv/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('build-webpack-xv/test/smoke/template/src/index/index.js'), true);
+    assert.equal(baseConfig.entry.search.indexOf('build-webpack-xv/test/smoke/template/src/search/index.js'), true);
   });
 });
